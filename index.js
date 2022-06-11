8c045efb97839f523a4c3b58cf2bf4ee8b6c9f70
MarisolM09 <mmendez9980@gmail.com>
2022-06-10 19:24:26 -0500

    first get

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-const { users } = require('./state')
+// this connects to our folder state.js
+const { users } = require('./state');
 
 /* BEGIN - create routes here */
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.get('/users/1', (req, res) => {
+  
+})
+
 
 
 /* END - create routes here */

86a3b2720065c375b38275da841708eed4b7ff3a
MarisolM09 <mmendez9980@gmail.com>
2022-06-12 20:11:37 -0500

    get delete

--- index.js
+++ index.js
@@ -3,20 +3,53 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+// middleware from express "body-parser"
+app.use(express.json());
+
 // this connects to our folder state.js
 const { users } = require('./state');
 
 /* BEGIN - create routes here */
+let newUser = {
+  "id": 6,
+  "name": "Max Jason",
+  "occupation": "Pooper Scooper",
+  "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+}
 
-app.get('/users', (req, res) => {
-  res.json(users);
+
+// Gets all users
+app.get('/users', (req, res) => res.json(users));
+
+
+// Gets 1 user
+app.get('/users/:id', (req, res) => {
+  res.json(users.filter(user => user.id === parseInt(req.params.id)))
+});
+
+
+// added new User with Post
+app.post('/users', (req, res) => {
+  const length = users.length;
+  const newPerson = {
+    id: length + 1,
+    ...req.body
+  }
+  users.push(newPerson)
+  res.json(users)
 })
 
-app.get('/users/1', (req, res) => {
+// Update user info
+app.put('/users/:id', (req, res) => {
+  let id = +req.params.id;
+  let body = req.body;
+});
+
+// Delete User
+app.delete('/users/:id', (req, res) => {
   
-})
-
-
+  
+});
 
 /* END - create routes here */
 
